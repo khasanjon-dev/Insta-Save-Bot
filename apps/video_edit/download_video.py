51b41d521f47277885282a3430ad7a47f844bc8c
Khasanjon <khasanjon.eng@gmail.com>
2023-07-14 12:17:01 +0500

    save video ready

--- apps/video_edit/download_video.py
+++ apps/video_edit/download_video.py
@@ -1,41 +1,20 @@
-import os
-import re
-
-from dotenv import load_dotenv
-from instaloader import Instaloader, Profile
-
-load_dotenv('../../.env')
-USERNAME = os.getenv('USERNAME_INSTA')
+import instaloader
 
 
-def download_latest_video(username):
-    insta = Instaloader()
-    profile = Profile.from_username(insta.context, username)
-    posts = profile.get_posts()
-    latest_post = next(posts, None)
-    while latest_post and not latest_post.is_video:
-        latest_post = next(posts, None)
-    if latest_post:
-        insta.download_post(latest_post, target='today')
-        print('Downloaded!')
-    else:
-        print('No videos found.')
+def download_instagram_video(url):
+    loader = instaloader.Instaloader()
+    try:
+        post = instaloader.Post.from_shortcode(loader.context, url.split('/')[-2])
+        loader.download_post(post, target='media')
+        print("Video downloaded successfully!")
+    except instaloader.exceptions.ProfileNotExistsException:
+        print("Profile not found.")
+    except instaloader.exceptions.PrivateProfileNotFollowedException:
+        print("Cannot download videos from private accounts.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 
-def is_valid_instagram_link(link):
-    pattern = r'^https?://(www\.)?instagram\.com/p/[^/]+/?$'
-    return re.match(pattern, link)
+video_url = 'https://www.instagram.com/p/CuePqiwMwmz/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=='
 
-
-def handle_input(input_value):
-    if is_valid_instagram_link(input_value):
-        # Extract the username from the link
-        username = input_value.split('/')[-2]
-        download_latest_video(username)
-    else:
-        download_latest_video(input_value)
-
-
-# Prompt the user for input
-input_value = input('Enter an Instagram username or video link: ')
-handle_input(input_value)
+download_instagram_video(video_url)
