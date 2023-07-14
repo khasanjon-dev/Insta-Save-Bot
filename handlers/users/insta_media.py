3ab6bfbd31f58695c1993e0e3fc5eb97ea19d9bb
Khasanjon <khasanjon.eng@gmail.com>
2023-07-14 19:16:17 +0500

    add aiogram template

--- handlers/users/insta_media.py
+++ handlers/users/insta_media.py
@@ -1,19 +1,10 @@
-import os
 import re
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from dotenv import load_dotenv
+from aiogram import types
 from instaloader import Instaloader, Post
 
-load_dotenv('../../.env')
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-BOT_USERNAME = os.getenv('BOT_USERNAME')
-
-bot = Bot(token=BOT_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+from data.config import BOT_USERNAME
+from loader import dp
 
 
 @dp.message_handler()
@@ -25,13 +16,14 @@ async def send_media(message: types.Message):
         loader = Instaloader()
         i = 0
         post = Post.from_shortcode(loader.context, shortcode)
-        for i, _post in enumerate(post.get_sidecar_nodes()):
+        for _post in post.get_sidecar_nodes():
             if _post.is_video:
                 video_url = _post.video_url
                 await message.answer_video(types.InputFile.from_url(video_url), caption=BOT_USERNAME)
             else:
                 photo_url = _post.display_url
                 await message.answer_photo(types.InputFile.from_url(photo_url), caption=BOT_USERNAME)
+            i += 1
         if i == 0:
             if post.is_video:
                 video_url = post.video_url
@@ -49,9 +41,3 @@ def extract_shortcode(url):
     if match:
         return match.group(1)
     return None
-
-
-if __name__ == '__main__':
-    from aiogram import executor
-
-    executor.start_polling(dp, skip_updates=True)
