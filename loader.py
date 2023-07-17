313e08591ba16d9e4acd5906537a8e5c8638701c
Khasanjon <khasanjon.eng@gmail.com>
2023-07-17 23:04:31 +0500

    update file

--- loader.py
+++ loader.py
@@ -3,6 +3,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML, proxy='http://proxy.server:3128')
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
