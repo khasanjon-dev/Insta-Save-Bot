665b050bfc3649e2cd2eaeba32bda1acf90207a1
Khasanjon <khasanjon.eng@gmail.com>
2023-08-30 07:50:21 +0500

    update models.py

--- utils/db_api/models.py
+++ utils/db_api/models.py
@@ -9,7 +9,7 @@ from utils.db_api.db.base_class import Base
 class Users(Base):
     first_name: Mapped[str] = mapped_column(String(250))
     last_name: Mapped[str] = mapped_column(String(250), nullable=True)
-    username: Mapped[str] = mapped_column(String(250), unique=True)
+    username: Mapped[str] = mapped_column(String(250), unique=True, nullable=True)
     telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True)
     updated_at: Mapped[datetime] = mapped_column(DateTime, onupdate=func.current_timestamp(), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
